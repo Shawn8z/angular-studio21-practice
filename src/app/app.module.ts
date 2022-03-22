6822cecc3c4a34809e3e30bd494709f8ee5e70d0
Shawn <lcorel86@gmail.com>
2022-03-21 19:54:24 -0500

    28.8.3.4v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CrewComponent } from './crew/crew.component';
+import { ExperimentComponent } from './experiment/experiment.component';
+import { EquipmentComponent } from './equipment/equipment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CrewComponent
+    CrewComponent,
+    ExperimentComponent,
+    EquipmentComponent
   ],
   imports: [
     BrowserModule
